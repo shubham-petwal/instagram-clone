511cb1b70b60477748cc4032ca43ced4672088ee
Surjeet Singh <surjeets@geekyants.com>
2022-10-25 11:33:57 +0530

    added env variables

--- frontend/src/firebaseSetup.ts
+++ frontend/src/firebaseSetup.ts
@@ -1,12 +1,12 @@
 import firebase from "firebase/compat/app"
 import "firebase/compat/auth"
 const firebaseConfig = {
-    apiKey: "AIzaSyB0VyC346tAfEqFHMAjbr_tkK1W7MgM-Hc",
-    authDomain: "instagram-clone-44275.firebaseapp.com",
-    projectId: "instagram-clone-44275",
-    storageBucket: "instagram-clone-44275.appspot.com",
-    messagingSenderId: "891143983058",
-    appId: "1:891143983058:web:c053efbd5962a04815020b"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_MESSAGING_APP_ID
 }; 
 firebase.initializeApp(firebaseConfig);
 
